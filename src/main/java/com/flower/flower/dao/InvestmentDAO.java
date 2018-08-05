b8930378c2e8a6e5a06635a6bb78ae899096fe58
yujiezhang <845075906@qq.com>
2018-08-05 12:13:53 +0800

    something

--- src/main/java/com/flower/flower/dao/InvestmentDAO.java
+++ src/main/java/com/flower/flower/dao/InvestmentDAO.java
@@ -2,6 +2,8 @@ package com.flower.flower.dao;
 
 import com.flower.flower.bean.investment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface InvestmentDAO extends JpaRepository<investment,Integer> {
 }
--- src/main/java/com/flower/flower/dao/ProductDAO.java
+++ src/main/java/com/flower/flower/dao/ProductDAO.java
@@ -2,6 +2,8 @@ package com.flower.flower.dao;
 
 import com.flower.flower.bean.product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface ProductDAO extends JpaRepository<product,Integer> {
 }
